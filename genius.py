fa1c3bebee653d57c4d4e310c8ea9b569a3c2c90
Linus045 <linusw20@gmail.com>
2020-04-28 22:46:44 +0200

    Fixed crash when no lyrics could be found

--- genius.py
+++ genius.py
@@ -5,5 +5,8 @@ import lyricsgenius
 def getLyric(songname, artist):
     apiKey = os.getenv('GENIUS_ACCESS_TOKEN')
     genius = lyricsgenius.Genius(apiKey)
-    song = genius.search_song(songname, artist, get_full_info=False)
-    return song.lyrics
+    try:
+        song = genius.search_song(songname, artist, get_full_info=False)
+        return song.lyrics
+    except:
+        return "No lyrics found..."
