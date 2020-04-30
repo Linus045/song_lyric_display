5be8be7a78391426660d509cefd08aa7ab43d26e
Linus045 <lwillms88@gmail.com>
2020-04-30 19:55:25 +0200

    Filtering out stuff in brackets when searching for song name

--- genius.py
+++ genius.py
@@ -6,6 +6,14 @@ def getLyric(songname, artist):
     apiKey = os.getenv('GENIUS_ACCESS_TOKEN')
     genius = lyricsgenius.Genius(apiKey)
     try:
+        bracketStart = songname.find("(")
+        bracketEnd = songname.find(")")
+        while (bracketStart != -1) and (bracketEnd != -1):
+            songname = songname[:bracketStart] + songname[bracketEnd+1:]
+            songname = songname.strip()
+            bracketStart = songname.find("(")
+            bracketEnd = songname.find(")")
+        songname = songname.replace('  ', ' ')
         song = genius.search_song(songname, artist, get_full_info=False)
         return song.lyrics
     except:
