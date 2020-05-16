1ad299fb2bfff310660ffbc53b2b219f72f130c2
Linus045 <lwillms88@gmail.com>
2020-05-16 19:34:55 +0200

    When mouse is hovering over box at the top, slide it's content to the left when it exceeds the box width

--- recentSongsRenderer.py
+++ recentSongsRenderer.py
@@ -15,6 +15,7 @@ class RecentSongsRenderer:
         self.cover_urls_img_data = {}
         self.highlightedIdx = -1
         self.time = 0
+        self.texttime = 0
         self.animate = True
         self.setWidth(width)
 
@@ -26,12 +27,28 @@ class RecentSongsRenderer:
         text_artist = self.font.render(artist, True, self.color_file.getColor('recentlyplayed.text.artist'), None)
 
         rect = pygame.Rect(pos[0], pos[1], size[0], size[1])
+        timeoffset = 0
         if highlighted:
             pygame.draw.rect(surface, self.color_file.getColor('recentlyplayed.highlighted'), rect)
+
+            title_diff = max(0,(textPos[0] - pos[0] + text_song.get_width()) - size[0])
+            artists_diff = max(0,(textPos[0] - pos[0] + text_artist.get_width()) - size[0])
+            needed_offset = max(title_diff, artists_diff)
+            if needed_offset > 0:
+                extra_space = 8
+                timeoffset = min(needed_offset + extra_space, self.texttime % (needed_offset + extra_space + 50))
         else:
             pygame.draw.rect(surface, self.color_file.getColor('recentlyplayed.background'), rect)
-        surface.blit(text_song, (textPos[0] + 3,textPos[1]))
-        surface.blit(text_artist, (textPos[0] + 3, textPos[1] + self.font.get_height() + 2))
+        clippingBox = pygame.Rect(textPos[0], textPos[0], size[0], size[1])
+        text_song.set_clip(clippingBox)
+        text_artist.set_clip(clippingBox)
+
+        textBox = pygame.Surface((size[0] - (textPos[0] - pos[0] + 5), size[1]))
+        textBox.set_colorkey(self.color_file.getColor('TRANSPARENT_KEY_COLOR'))
+
+        textBox.blit(text_song,(-timeoffset,0))
+        textBox.blit(text_artist,(-timeoffset, self.font.get_height() + 2))
+        surface.blit(textBox, (textPos[0] + 3, textPos[1]))
         return rect
 
     def try_draw_cover_img(self, surface, pos, size, song_uri):
@@ -70,6 +87,9 @@ class RecentSongsRenderer:
         surface.set_colorkey(self.color_file.getColor('TRANSPARENT_KEY_COLOR'))
         if self.animate:
             self.time = self.time + 0.3
+            self.texttime = 0
+        else:
+            self.texttime = self.texttime + 0.5
 
         if self.recently_played_songs:
             for songIdx in range(len(self.recently_played_songs)):
